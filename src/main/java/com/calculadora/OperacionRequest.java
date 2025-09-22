8a7c627ea14c4e8d91a58774ff84ccde99bf5a74
JoseLuisCalizaya <joseluisataque@gmail.com>
2025-09-21 19:35:11 -0500

    Creacion del archivo .java controlador

---
+++ src/main/java/com/calculadora/OperacionRequest.java
@@ -0,0 +1,19 @@
+package com.calculadora.model;
+
+public class OperacionRequest {
+    private double num1;
+    private double num2;
+    private String operacion; 
+
+    // Getters y Setters
+    public double getNum1() { return num1; }
+    public void setNum1(double num1) { this.num1 = num1; }
+
+    public double getNum2() { return num2; }
+    public void setNum2(double num2) { this.num2 = num2; }
+
+    public String getOperacion() { return operacion; }
+    public void setOperacion(String operacion) { this.operacion = operacion; }
+}
+
+
